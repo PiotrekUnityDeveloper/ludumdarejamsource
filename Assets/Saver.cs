bb8759a8af05d98222a67e563b06c95711fe1e65
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-04-02 13:42:06 +0200

    complete saving and loading script

--- Assets/Saver.cs
+++ Assets/Saver.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Saver : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject camera;
     public List<GameObject> reloadableObjects = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -91,6 +92,18 @@ public class Saver : MonoBehaviour
             }
 
         }
+
+        //cam pos
+        PlayerPrefs.SetFloat("cameraXpos", camera.transform.position.x);
+        PlayerPrefs.SetFloat("cameraYpos", camera.transform.position.y);
+
+        //cam rot
+        PlayerPrefs.SetFloat("cameraZrot", camera.transform.rotation.z);
+
+        //cam offsets
+        PlayerPrefs.SetFloat("cameraXoffset", camera.GetComponent<CamControl>().camoffset.x);
+        PlayerPrefs.SetFloat("cameraYoffset", camera.GetComponent<CamControl>().camoffset.y);
+        //Z offset is always set to -10 so no need to save it
     }
 
     public void LoadLocalGame()
@@ -102,5 +115,31 @@ public class Saver : MonoBehaviour
         //loading player velocity
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(PlayerPrefs.GetFloat("playerXvel", 0), PlayerPrefs.GetFloat("playerYvel", 0));
         //done for the player
+        //now other stuff
+
+        foreach (GameObject g in reloadableObjects)
+        {
+            //other stuff
+            g.transform.position = new Vector2(PlayerPrefs.GetFloat(g.name + "Xpos", 0), PlayerPrefs.GetFloat(g.name + "Ypos", 0));
+            //now rotation
+            g.transform.rotation = Quaternion.Euler(g.transform.rotation.x, g.transform.rotation.y, PlayerPrefs.GetFloat(g.name + "Zrot", 0));
+            //velocity
+            g.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(PlayerPrefs.GetFloat(g.name + "Xvel", 0), PlayerPrefs.GetFloat(g.name + "Yvel", 0));
+            //and finally, the gravity
+            g.GetComponent<Rigidbody2D>().gravityScale = PlayerPrefs.GetFloat(g.name + "gravity", 0);
+            //is the object enabled?
+            //retrieve the bool first
+            bool b = PlayerPrefs.GetInt(g.name + "enabled", 0) > 0;
+            g.gameObject.SetActive(b); //setting the object to the variable val
+        }
+
+        //camera
+        //load position of the camera
+        camera.transform.position = new Vector2(PlayerPrefs.GetFloat("cameraXpos", 0), PlayerPrefs.GetFloat("cameraYpos", 0));
+        //load rotation of the caemraa
+        camera.transform.rotation = Quaternion.Euler(camera.transform.rotation.x, camera.transform.rotation.y, PlayerPrefs.GetFloat("cameraZrot", 0));
+        //load offset of the cameracontroll script (attached)
+        camera.GetComponent<CamControl>().camoffset = new Vector3(PlayerPrefs.GetFloat("cameraXoffset", 0), PlayerPrefs.GetFloat("cameraYoffset", 0), -10);
+        
     }
 }
