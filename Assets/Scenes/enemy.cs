428e8a64706d6bfcb7b020797ec1e2833e905ef7
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2022-04-03 18:08:21 +0200

    stuff

--- Assets/Scenes/enemy.cs
+++ Assets/Scenes/enemy.cs
@@ -13,11 +13,13 @@ public class enemy : MonoBehaviour
     public float detectionrange;
     public float rotaddiction = 0;
 
+    public bool isdashingenemy;
+
     // Start is called before the first frame update
     void Start()
     {
         lockedpos = this.transform.position;
-        move = false;
+        //move = false;
         StartCoroutine(delaylockedpossetter());
     }
 
@@ -29,13 +31,53 @@ public class enemy : MonoBehaviour
             this.transform.position = lockedpos;
         }
 
-        detectplayer();
+        //detectplayer();
     }
 
     //public static RaycastHit2D rh;
 
+    private void FixedUpdate()
+    {
+        fixeddetection();
+    }
+
+    public void fixeddetection()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, detectionrange);
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.left);
+        RaycastHit2D hit3 = Physics2D.Raycast(transform.position, Vector2.up);
+        RaycastHit2D hit4 = Physics2D.Raycast(transform.position, Vector2.down);
+
+        Debug.DrawLine(this.transform.position, player.transform.position, Color.blue, 0.01f);
+
+        move = false;
+
+        if(hit.collider.gameObject.tag == "Player" && hit.collider.gameObject.layer == 3) //found a player!
+        {
+            move = true;
+        }
+
+        if (hit2.collider.gameObject.tag == "Player" && hit.collider.gameObject.layer == 3) //found a player!
+        {
+            move = true;
+        }
+
+        if (hit3.collider.gameObject.tag == "Player" && hit.collider.gameObject.layer == 3) //found a player!
+        {
+            move = true;
+        }
+
+        if (hit4.collider.gameObject.tag == "Player" && hit.collider.gameObject.layer == 3) //found a player!
+        {
+            move = true;
+        }
+
+        //done i think
+    }
+
     public void detectplayer()
     {
+        
         this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z - 90);
         RaycastHit2D rh = Physics2D.Raycast(this.transform.position, transform.right, detectionrange, 1, playermask);
         //this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, this.transform.rotation.y, this.transform.rotation.z - 90);
@@ -67,7 +109,31 @@ public class enemy : MonoBehaviour
             {
                 //move = false;
             }
+
         }
+
+        if(Vector2.Distance(this.transform.position, player.transform.position) < 5)
+        {
+            StartCoroutine(dash());
+            move = false;
+            StartCoroutine(delaymovement());
+        }
+    }
+
+    public IEnumerator dash()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            yield return new WaitForSecondsRealtime(0.4f);
+            this.transform.position = transform.up * -0.1f;
+            Debug.Log("dashing!");
+        }
+    }
+
+    public IEnumerator delaymovement()
+    {
+        yield return new WaitForSecondsRealtime(1.9f);
+        move = true;
     }
 
     public IEnumerator delaylockedpossetter()
